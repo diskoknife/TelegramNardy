99e54d7e3a91e3d364171709b2db0d76b65321c5
Vyacheslav Shchurov <vschyurov@psystems.biz>
2021-03-03 17:45:22 -0500

    refactor(Create_map.py): deleted some func

--- src/Create_map.py
+++ src/Create_map.py
@@ -1,16 +1,10 @@
 class Create_map:
-    def __init__(self, desk_length=23, white=15, black=15):
+    def __init__(self, desk_length=23):
         self.desk_length = desk_length
-        self.white = white
-        self.black = black
-
 
     def create_map(self):
         matr = [[0 for i in range(self.desk_length)] in range(2)]
         return matr
 
-    def write_map(self):
-        with open('map', 'w') as f:
-            f.write(self.create_map())
-
-# TODO:split an existing matrix as a 2 lists
+    def write_map(self, fis_num=15, black = "Black", white = "White"):
+        line_sequence = {}
