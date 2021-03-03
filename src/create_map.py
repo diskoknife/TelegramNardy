495b672b5177f327f25b7e5030e69a97af1c234b
Vyacheslav Shchurov <vschyurov@psystems.biz>
2021-03-03 18:38:18 -0500

    (refactor)Class: we don't need no class control

---
+++ src/create_map.py
@@ -0,0 +1,11 @@
+null_dict = {}
+
+
+def create_map(desk_length=23, fis_num=15, black="Black", white="White"):
+    for i in range(desk_length):
+        null_dict.setdefault(i+1, None)
+    null_dict[1] = [white for j in range(fis_num)]
+    null_dict[13] = [black for k in range(fis_num)]
+    return null_dict
+
+print(create_map())
