b18b5bf4d16cc765b588f8694ee2c69105545225
Vyacheslav Shchurov <vschyurov@psystems.biz>
2021-03-03 18:57:15 -0500

    Refactor(map):

--- src/create_map.py
+++ src/create_map.py
@@ -7,5 +7,3 @@ def create_map(desk_length=23, fis_num=15, black="Black", white="White"):
     null_dict[1] = [white for j in range(fis_num)]
     null_dict[13] = [black for k in range(fis_num)]
     return null_dict
-
-print(create_map())
