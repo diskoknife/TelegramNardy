f6c2a7a87aa5029677317e3a224f97465d56a8cb
Vyacheslav Shchurov <vschyurov@psystems.biz>
2021-03-04 01:50:04 -0500

    something important

---
+++ throw_cube.py
@@ -0,0 +1,12 @@
+"""
+
+*Put some description for this file here*
+
+"""
+import random
+
+
+def throw_cube():
+    cube1 = random.randint(1, 6)
+    cube2 = random.randint(1, 6)
+    return cube1, cube2
