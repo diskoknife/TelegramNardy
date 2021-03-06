c1d77340eefd1630bdefe1968d8f32598d67c514
Vyacheslav Shchurov <vschyurov@psystems.biz>
2021-03-06 17:37:29 -0500

    feat(main): test def

--- Controllers/BattleController.py
+++ Controllers/BattleController.py
@@ -3,13 +3,10 @@
 *Put some description for this file here*
 
 """
-from Battle.Battle import Battle
-from Battle.Checker import Checker
 from Controllers.throw_cube import throw_cube
 
 
 class BattleController:
     cube_a, cube_b = throw_cube()
     color = "White"
-    Battle()
 
---
+++ main.py
@@ -0,0 +1,17 @@
+"""
+
+*Put some description for this file here*
+
+"""
+from Controllers.BattleController import BattleController
+party = BattleController
+
+
+def main():
+    party
+    print(party.cube_a, party.cube_b)
+
+
+
+if __name__ == '__main__':
+    main()
